9076d8b4319706963fe68fca3a5be1fb03e947fb
Prashant Patil <prashant.patil@niyuj.com>
2015-11-26 13:33:26 +0530

    Update common_rails_date_datetime_conditions_generator_mixin.rb

--- lib/wice/columns/common_rails_date_datetime_conditions_generator_mixin.rb
+++ lib/wice/columns/common_rails_date_datetime_conditions_generator_mixin.rb
@@ -7,14 +7,15 @@ module Wice
         conditions = [[]]
         if opts[:fr]
           conditions[0] << " #{@column_wrapper.alias_or_table_name(table_alias)}.#{@column_wrapper.name} >= ? "
-          conditions << opts[:fr].to_datetime
+          #conditions << opts[:fr].to_datetime
+          conditions << Time.zone.parse(opts[:fr]).utc
           #Convert user time zone to utc 
           #conditions << Time.zone.parse(opts[:fr]).utc.to_s(:form_format)
         end
 
         if opts[:to]
           conditions[0] << " #{@column_wrapper.alias_or_table_name(table_alias)}.#{@column_wrapper.name} <= ? "
-          conditions << (opts[:to].to_datetime + 1)
+          conditions << (Time.zone.parse(opts[:to]).utc + 1)
         end
 
         return false if conditions.size == 1
